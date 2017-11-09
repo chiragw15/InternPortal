5af3767b1eff230a71ee4e9707855c5368f2e12a
cc15 <chiragwadhera15@gmail.com>
2017-11-09 19:00:53 +0530

    setup db4

--- app.py
+++ app.py
@@ -14,13 +14,10 @@ from urllib.parse import urlparse, urlunparse
 bottle.debug(True)
 #graph = Graph("http://neo4j:Chirag@1234@localhost:7474/db/data/")
 
-url = urlparse(os.environ.get("GRAPHENEDB_URL"))
-url_without_auth = urlunparse((url.scheme, "{0}:{1}".format(url.hostname, url.port), '', None, None, None))
-user = url.username
-password = url.password
-
-authenticate(url_without_auth, user, password)
-graph = Graph(url_without_auth)
+graphenedb_url = os.environ.get("GRAPHENEDB_BOLT_URL")
+graphenedb_user = os.environ.get("GRAPHENEDB_BOLT_USER")
+graphenedb_pass = os.environ.get("GRAPHENEDB_BOLT_PASSWORD")
+graph = Graph(graphenedb_url, user=graphenedb_user, password=graphenedb_pass, bolt = True, secure = True, http_port = 24789, https_port = 24780)
 
 @get("/")
 def get_index():
@@ -41,7 +38,7 @@ def get_registerJSON():
     except KeyError:
         return {"respose": "Some error occurred"}
     else:
-        results = graph.cypher.execute(
+        results = graph.run(
         "create (n:professor{name:" + "\"" + name + "\"" + "," + 
                             "research:" + "\"" + research + "\"" + "," +
                             "email:" + "\"" + email + "\"" + "," +
